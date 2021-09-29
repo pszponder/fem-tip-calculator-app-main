2b77b0d1688cc9cd3d55e930c65a5611268c65d8
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2021-09-29 16:12:39 -0600

    add event listener to compute output and update tip and total amount on the screen

--- src/js/script.js
+++ src/js/script.js
@@ -16,15 +16,20 @@ const inputBill = document.querySelector("#bill");
 
 // Add event handler to capture input
 function handleInputBill(event) {
+  // Extract value from input
   const inputStr = parseFloat(event.currentTarget.value);
 
+  // If input is valid,
+  // update bill value
+  // determine if output can be computed
   if (inputStr) {
     inputs.bill = inputStr;
+    canCompute(inputs);
   }
 }
 
-// Add event handler function to clear the custom tip input
-function handleBillClear() {
+// Add function to clear the custom tip input
+function clearBill() {
   inputBill.value = "";
 }
 
@@ -42,21 +47,21 @@ const tipButtons = document.querySelectorAll("button.input__tip-btn");
 // const tipBtn25 = document.querySelector("#p25");
 // const tipBtn50 = document.querySelector("#p50");
 
-// Create an event handler function to remove the active state from all buttons
-function handleRemoveTipActive(buttons) {
+// Create an function to remove the active state from all buttons
+function clearTipActive(buttons) {
   // Remove active state class from each button in buttons
   buttons.forEach((button) => {
     button.classList.remove("input__tip-btn--active");
   });
 
   // Also clear the custom tip button
-  handleBtnTipClear();
+  clearTipBtn();
 }
 
 // Create an event handler function to toggle an active state to the selected button
 function handleToggleTipActive(event) {
   // Remove the active class on all tip buttons
-  handleRemoveTipActive(tipButtons);
+  clearTipActive(tipButtons);
 
   // Add the active class on the buttons which was clicked
   event.currentTarget.classList.add("input__tip-btn--active");
@@ -70,6 +75,9 @@ function handleBtnTip(event) {
 
   // Update the value of the inputs object tip with percent
   inputs.tip = percent;
+
+  // Determine if output can be computed
+  canCompute(inputs);
 }
 
 // Add event handlers to all buttons
@@ -95,11 +103,14 @@ function handleBtnTipCustom(event) {
 
     // Update the value of the inputs object tip with percent
     inputs.tip = percent;
+
+    // Determine if output can be computed
+    canCompute(inputs);
   }
 }
 
-// Add event handler function to clear the custom tip input
-function handleBtnTipClear() {
+// Add function to clear the custom tip input
+function clearTipBtn() {
   tipBtnCustom.value = "";
 }
 
@@ -115,34 +126,67 @@ const inputPeople = document.querySelector("#num-people");
 function handleInputPeople(event) {
   const inputStr = parseInt(event.currentTarget.value);
 
+  // If input is valid
   if (inputStr) {
     inputs.people = inputStr;
+
+    // Determine if output can be computed
+    canCompute(inputs);
   }
 }
 
 // Add event handler function to clear the custom tip input
-function handleNumPeopleClear() {
+function clearNumPeople() {
   inputPeople.value = "";
 }
 
 // Add event listener to the bill input
 inputPeople.addEventListener("keyup", handleInputPeople);
 
-// =======
-// OUTPUTS
-// =======
-
-// ===================
-// TIP AMOUNT / PERSON
-// ===================
+// ====================================
+// TIP AMOUNT / PERSON + TOTAL / PERSON
+// ====================================
 // Store the tip amount / person output in a variable
 const amountTip = document.querySelector("#tip-amount");
 
-// ==============
-// TOTAL / PERSON
-// ==============
 // Store the total amount / person output in a variable
 const amountTotal = document.querySelector("#total-amount");
+
+// Add event handler function to clear outputs
+function clearOutputs() {
+  // Reset value of the Tip Amount
+  amountTip.innerText = "$0.00";
+
+  // Reset value of the Total Amount
+  amountTotal.innerText = "$0.00";
+}
+
+// =======
+// OUTPUTS
+// =======
+// Create function that will check if the outputs can be computed
+function canCompute(inputs) {
+  // Exit function if any value in inputs is 0
+  for (const key in inputs) {
+    if (inputs[key] === 0) {
+      return;
+    }
+  }
+
+  // Run the computeTip function
+  computeTip(inputs);
+}
+
+// Create function that will compute the tip
+function computeTip(inputs) {
+  amountTip.innerText = `${
+    Math.round(((inputs.bill * inputs.tip) / inputs.people) * 100) / 100
+  }`;
+
+  amountTotal.innerText = `${
+    Math.round(((inputs.bill * (1 + inputs.tip)) / inputs.people) * 100) / 100
+  }`;
+}
 
 // ============
 // RESET BUTTON
@@ -158,32 +202,23 @@ function handleReset() {
   }
 
   // Reset value of the Bill Input
-  handleBillClear();
+  clearBill();
 
   // Remove active state of tip buttons
-  handleRemoveTipActive(tipButtons);
+  clearTipActive(tipButtons);
 
   // Reset value of the custom tip field
-  handleBtnTipClear();
+  clearTipBtn();
 
   // Reset value of the Number of People
-  handleNumPeopleClear();
+  clearNumPeople();
 
-  // Reset value of the Tip Amount
-  amountTip.innerText = "$0.00";
-
-  // Reset value of the Total Amount
-  amountTotal.innerText = "$0.00";
+  // Reset Outputs
+  clearOutputs();
 }
 
 // Add event listener to the resetBtn using the reset() event handler
 resetBtn.addEventListener("click", handleReset);
-
-// Add an event handler to add a styling class to tip buttons
-
-// Add an event handler which takes the value of the selected tip button and uses it to compute the tip
-
-// Add event listeners to all tip buttons using the styling event handler
 
 /*
 Bill = $142.55
@@ -195,3 +230,7 @@ Total Amount / Person = $32.79
 Total Amount = Bill * (1 + tip) / numPeople
 Tip Amount = (Bill * tip ) / numPeople
 */
+
+// TODO: Force the inputs to clear on page reload (form.reset()?)
+// TODO: add red border and error message if user inputs 0 people into people text box
+// TODO: add animation to inputs if they are not valid (ex, if user enters a decimal number for people or enters words into any of the inputs)
