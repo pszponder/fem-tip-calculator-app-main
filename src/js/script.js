f170424d438b8f01ebc8bbe7ceee36ddfaecf2ea
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2021-09-29 09:01:06 -0600

    add notes to script.js

---
+++ src/js/script.js
@@ -0,0 +1,42 @@
+// ==========
+// BILL INPUT
+// ==========
+// Store the bill input element in a variable
+
+// ===========
+// TIP BUTTONS
+// ===========
+// Store the tip buttons in a variable
+
+// ================
+// CUSTOM TIP INPUT
+// ================
+// Store the custom tip button in a variable
+
+// ======================
+// NUMBER OF PEOPLE INPUT
+// ======================
+// Store the number of people input in a variable
+
+// ===================
+// TIP AMOUNT / PERSON
+// ===================
+// Store the tip amount / person output in a variable
+
+// ==============
+// TOTAL / PERSON
+// ==============
+// Store the total amount / person output in a variable
+
+// ============
+// RESET BUTTON
+// ============
+// Store the reset button in a variable
+
+// Add an event handler to add a styling class to tip buttons
+
+// Add an event handler which takes the value of the selected tip button and uses it to compute the tip
+
+// Add event listeners to all tip buttons using the styling event handler
+
+// Add an event handler to reset the tip calculator values
